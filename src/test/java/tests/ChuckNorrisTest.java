596287dca8a8fb278b7a4e3a8559113430ebd073
user <nadya11090@gmail.com>
2021-12-26 01:53:27 +0300

    added changes to ChuckNorrisTest

--- src/test/java/tests/ChuckNorrisTest.java
+++ src/test/java/tests/ChuckNorrisTest.java
@@ -2,12 +2,33 @@ package tests;
 
 import org.junit.Test;
 import pages.ChuckNorrisPage;
+import static org.hamcrest.Matchers.*;
 
 public class ChuckNorrisTest {
     ChuckNorrisPage chuckNorrisPage = new ChuckNorrisPage();
 
     @Test
     public void changingName(){
-    chuckNorrisPage.jokeNameLastname("Vanya", "Ivanov");
+    chuckNorrisPage.jokeNameLastname("Vanya", "Ivanov").body("type", equalTo("success"));
+    }
+
+    @Test
+    public void changingNameContentTypeTest() {
+        chuckNorrisPage.jokeNameLastname("Vanya", "Ivanov").assertThat().header("Content-Type", "application/json");
+    }
+
+    @Test
+    public void changingNameContainsNameTest(){
+        chuckNorrisPage.jokeNameLastname("Vanya", "Ivanov").body("value.joke", containsString("Vanya"));
+    }
+
+    @Test
+    public void randomJokeTest() {
+        chuckNorrisPage.randomJoke().body("value.joke", containsString("Chuck"));
+    }
+
+    @Test
+    public void randomJokeContentTest() {
+        chuckNorrisPage.randomJoke().assertThat().header("Content-Type", "application/json");
     }
 }
